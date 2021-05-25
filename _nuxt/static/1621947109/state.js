ca01d1a2de271d32706a1f935dcb235b8ac13bca
yusukekozen <yusuke.kozen@gmail.com>
2021-05-25 21:52:02 +0900

    e5f851b

---
+++ _nuxt/static/1621947109/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fkozen_tec_web\u002F_nuxt\u002Fstatic\u002F1621947109",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fkozen_tec_web\u002F",assetsPath:"\u002Fkozen_tec_web\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d31c080a"}}}}(null));
